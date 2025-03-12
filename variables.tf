3104a82fe58e737f35465dd664d992cae749cf02
harshini <harshinipamarthi@gmail.com>
2025-03-12 15:38:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "PROJECT_ID" {
     description = "Project ID"
     type = string
-    //default = "harshini-project-452710"
+    default = "harshini-project-452710"
 }
 variable "REGION" {
     description = "Region for resources"
     type = string
-  //default = "us-east1"
+    default = "us-centra1"
 }
 variable "zone" {
     description = "Zone of the VM"
     type = string
-    //default = "us-east1-a"
+    default = "us-central1-a"
 }
 variable "machine_type" {
     description = "Machine type of the VM"
     type = string
-    //default = "n1-standard-1"
+    default = "e2-medium"
 }
 variable "image" {
     description = "Image of the VM"
     type = string
-    //default = "centos-cloud/centos-stream-9-v20250123"
+    default = "centos-cloud/centos-stream-9-v20250123"
 }
